3cddcd49566dc902a48f8112d1a648cb5884f105
ManyKaufmann <manynouk.kaufmann@stud.hslu.ch>
2021-09-30 09:41:40 +0200

    update

---
+++ content/day01/tst/sketch3.js
@@ -0,0 +1,29 @@
+var offset = 0;
+// Default P5 setup function
+function setup() {
+  createCanvas(600, 400);
+  // video = createCapture(VIDEO);
+  //video.hide();
+}
+
+// Default P5 draw loop function
+function draw() {
+  background(0);
+
+  //stroke(255);
+  //strokeWeight(2);
+
+  for (var x = 0; x <= width; x += 20) {
+    for (var y = 0; y <= innerHeight; y += 60) {
+      fill(random(200), 120, random(70));
+      //ellipse(x - offset, y - offset, 25, 25);
+      //rect(x - offset, y - offset, 100, 150);
+      //triangle(30, x-offset, 360, 50, 86, 120)
+      triangle(x - offset, y - offset, -40, -60, -86, -40);
+      //triangle(30, 30, 400, y-offset, 86, 400)
+
+      //circle(x-offset, y-offset, 60);
+    }
+    //image(video, 0, 0, width, height);
+  }
+}
